edb26bf18346d07cf19635d5c91f2f01e3f3a04d
Glen Roberts <glensroberts@gmail.com>
2016-03-08 02:19:24 -0500

    Initial Commit

---
+++ ToggleImgButton/app/src/main/java/com/inzanedevelopment/roberts/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.inzanedevelopment.roberts.myfirstandroidapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+     public void clickFunction(View view) {
+         ImageView myImage = (ImageView) findViewById(R.id.picture);
+
+        if (myImage.getTag().equals("harley1")) {
+            myImage.setImageResource(R.drawable.harley2);
+            myImage.setTag("harley2");
+        }else{
+            myImage.setImageResource(R.drawable.harley1);
+            myImage.setTag("harley1");
+        }
+
+
+
+        //Toast.makeText(getApplicationContext(), "Hi GoldenKnight!",Toast.LENGTH_LONG).show();
+
+        //Log.i("Info", "Button Tapped");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
